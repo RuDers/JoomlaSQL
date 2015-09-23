60d54d43f77cca85a20eab847dae2393bc8a65ef
RuDers <lordders93@hotmail.com>
2015-09-23 13:35:14 +0200

    Joomla

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-09-11 06:11:43 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2015-09-11T06:11:43+00:00	INFO	joomlafailure	Usuario no existe
+2015-09-11T06:11:54+00:00	INFO	joomlafailure	Usuario no existe
